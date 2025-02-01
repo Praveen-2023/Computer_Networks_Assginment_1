856d7fbd7a51e2a2938d631ae0987c4fcab30b86
Praveen-2023 <143311712+Praveen-2023@users.noreply.github.com>
2025-02-01 23:23:19 +0530

    Update part1.py

--- part1.py
+++ part1.py
@@ -44,5 +44,5 @@ def show_results():
 
 # Run the packet capture and display results after completion
 if __name__ == "__main__":
-    capture_packets()  # Start capturing packets for the defined duration
-    show_results()  # Display captured data and packet size statistics
+    capture_packets()  
+    show_results()  
